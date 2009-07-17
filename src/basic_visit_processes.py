a2d7ffbd42b6cd95553e1dd27040b0b63260b1df
Erik Kastman <erik@nelson.medicine.wisc.edu>
2009-07-17 08:24:57 -0500

    Commented, added some error checking.

--- src/basic_visit_processes.py
+++ src/basic_visit_processes.py
@@ -6,7 +6,6 @@ import os
 class Visit_directory:
     PREPROC_SCRIPT = '/Data/data1/lab_scripts/preproc_anat.sh '
     CREATE_CONTENTS_SCRIPT='/Data/data1/lab_scripts/create_contents.sh '
-    DEFAULT_COOKIE = 'study_info_cookie.py'
 
     def __init__(self, subid, raw_scans_directory, processed_scans_directory):
         self.subid = subid
@@ -14,25 +13,25 @@ class Visit_directory:
         self.processed_scans_directory = processed_scans_directory
         self.working_directory = os.path.join('/tmp/', os.path.basename(raw_scans_directory))
 
-        self.check_paths()
+        try:
+            self.check_paths()
+        except IOError as err:
+            print "There was a problem with permissions or paths: " + err
+            sys.exit(1)
 
+    # Basic Sanity & Directory Permissions Checking
     def check_paths(self):
-        # Raw Directory Must Exist and be Readable
+        # Ensure that Raw Directory Exists and is Readable
         if not os.path.exists(self.raw_scans_directory):
             raise IOError, "Visit raw directory " + self.raw_scans_directory + " does not exist."
         elif not os.access(self.raw_scans_directory, os.R_OK):
             raise IOError, "Cannot read visit raw directory: " + path
 
-        # Check any pahts that will be written to for correct permissions.
+        # Check any paths that will be written to for correct permissions.
         for path in [self.processed_scans_directory]:
             if os.path.exists(path):
                 if not os.access(path, os.W_OK): raise IOError, "Cannot write to: " + path
-
-    def check_preprocessed_scans_directory(self):
-        if not os.exists(self.processed_scans_directory):
-            print "Creating Processed Scans Directory " + path + "..."; os.makedirs(path)
-        elif not os.access(self.processed_scans_directory, os.W_OK):
-            raise IOError, "Cannot write to " + self.processed_scans_directory
+            else: print "Creating " + path + "..."; os.makedirs(path)
 
     # Create a copy of data on a local, quick disk and unzip all raw data inside.
     def prepare_working_directory(self, working_directory):
@@ -40,7 +39,7 @@ class Visit_directory:
             self.copyAndUnzip(self.raw_scans_directory, working_directory)
         else: print "Temporary working directory " + working_directory + "already exists."
 
-    # Figure out what scans are included in the visit.
+    # Scan the visit and determine what scans occured and need to be processed.
     # Eventually I will work in the sql database to scan that instead of
     # reparsing, but at the moment I'm just using the old bash script to
     # check file counts and scan series descriptions.
@@ -48,11 +47,24 @@ class Visit_directory:
         index_file_path = self.create_index_file(self.working_directory)
         if not os.path.exists(index_file_path): shutil.copy(index_file_path, self.raw_scans_directory)
 
+    # Creates and executes the old-school shell command for walking through dicoms and extracting 
+    # series descriptions and counts to create an index text file of scans in a visit.
+    def create_index_file(self, directory_to_scan, filename = None):
+        if not filename: filename = 'anat_list_' + self.subid + '.txt'
+        index_file_path = os.path.join(directory_to_scan, filename)
+        cmd = self.CREATE_CONTENTS_SCRIPT + " %s %s" % (directory_to_scan, index_file_path)
+        os.system(cmd)
+        #os.system("lpr " + anat_index_file)
+        return os.path.abspath(index_file_path)
+    
+    # Run basic preprocessing.  This currently just runs an old shell script, but will be
+    # updated to integrate with our imaging database and yaml processing configuration.
     def preprocess_each_scan(self):
         for recon_type in ['anat', 'fmri']:
             if not os.path.exists(os.path.join(self.processed_scans_directory, recon_type)):
                 self.recon(recon_type)
 
+    # Compiles Shell Command for reconstruction with preproc_anat.sh and runs it.
     def recon(self, recon_type, prefix = None, output_directory = None):
         if not output_directory:
             output_directory = os.path.join(self.processed_scans_directory, recon_type)
@@ -70,10 +82,15 @@ class Visit_directory:
     def tidy_up_raw_scans_directory(self):
         try:
             if not os.access(self.raw_scans_directory, os.W_OK):
-                raise IOError, "Raw directory " + path + " must be writable to zip files.  Try again as the raw user."
+                raise IOError, "Error: Raw directory " + path + " must be writable to zip files.  Try again as the raw user."
             else: self.zip(self.anatomicals_directory)
-        except IOError, msg:
-            print msg
+        except IOError as err:
+            print err
+
+    # Creates and executes a shell find command to recursively zip a directory, ignoring select small files
+    def zip(self, directory_to_zip):
+        print "Zipping " + directory_to_zip
+        os.system("find " + directory_to_zip + " -type f -not -name '*.txt' -not -name '*.yaml' -not -name '*.tar' -name '*' -exec bzip2 {} \;")
 
 
     # Remove the Working Directory
@@ -104,16 +121,3 @@ class Visit_directory:
                 file = os.path.join(path, f)
                 if file.endswith(".bz2"):
                     os.system("bunzip2 %s" % (file,))
-
-    def create_index_file(self, directory_to_scan, filename = None):
-        if not filename: filename = 'anat_list_' + self.subid + '.txt'
-        index_file_path = os.path.join(directory_to_scan, filename)
-        cmd = self.CREATE_CONTENTS_SCRIPT + " %s %s" % (directory_to_scan, index_file_path)
-        os.system(cmd)
-        #os.system("lpr " + anat_index_file)
-        return os.path.abspath(index_file_path)
-
-    def zip(self, directory_to_zip):
-        print "Zipping " + directory_to_zip
-        os.system("find " + directory_to_zip + " -type f -not -name '*.txt' -not -name '*.yaml' -not -name '*.tar' -name '*' -exec bzip2 {} \;")
-
